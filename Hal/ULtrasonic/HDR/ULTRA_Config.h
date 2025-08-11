5ee81750e57c7d5a96646715734f9665067cfd2e
Abdelwahab-Elordy <abdelwahabelordy@gmail.com>
2025-08-11 21:39:16 +0300

    Create ULTRA_Config.h

---
+++ Hal/ULtrasonic/HDR/ULTRA_Config.h
@@ -0,0 +1,15 @@
+/*
+ * ULTRA_Config.h
+ *
+ *  Created on: Aug 4, 2025
+ *      Author: tohami
+ */
+#ifndef HAL_ULTRASONIC_HDR_ULTRA_CONFIG_H_
+#define HAL_ULTRASONIC_HDR_ULTRA_CONFIG_H_
+
+
+#define ULTRA_TRIGGER_PORT     DIO_GROUPD
+#define ULTRA_TRIGGER_PIN      DIO_PIN7
+
+
+#endif /* HAL_ULTRASONIC_HDR_ULTRA_CONFIG_H_ */
