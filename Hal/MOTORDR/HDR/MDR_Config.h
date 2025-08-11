fcbbe6b99c62517678004aef397a58779f2f7cc7
Abdelwahab-Elordy <abdelwahabelordy@gmail.com>
2025-08-11 21:33:14 +0300

    Create MDR_Config.h

---
+++ Hal/MOTORDR/HDR/MDR_Config.h
@@ -0,0 +1,24 @@
+/*
+ * MDR_Config.h
+ *
+ *  Created on: Aug 4, 2025
+ *      Author: toham
+ */
+
+
+#ifndef HAL_MOTORDR_HDR_MDR_CONFIG_H_
+#define HAL_MOTORDR_HDR_MDR_CONFIG_H_
+
+#define MDR_Port DIO_GROUPC
+
+#define MDR_PA1 DIO_PIN0
+#define MDR_PA2 DIO_PIN1
+#define MDR_PB3 DIO_PIN2
+#define MDR_PB4 DIO_PIN3
+
+#define MOTOR_PWM_PORT DIO_GROUPB
+#define MOTOR_PWM_PIN  DIO_PIN3   // OC0 (PB3)
+
+
+
+#endif /* HAL_MOTORDR_HDR_MDR_CONFIG_H_ */
