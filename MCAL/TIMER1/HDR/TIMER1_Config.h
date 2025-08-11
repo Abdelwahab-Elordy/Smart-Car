49cc87947eea07cc69d6bfe968bc86d0c7fd07cb
Abdelwahab-Elordy <abdelwahabelordy@gmail.com>
2025-08-11 20:58:52 +0300

    Create TIMER1_Config.h

---
+++ MCAL/TIMER1/HDR/TIMER1_Config.h
@@ -0,0 +1,13 @@
+/*
+ * TIMER1_Config.h
+ *
+ *  Created on: Jul 28, 2025
+ *      Author: tohami
+ */
+
+#ifndef MCAL_TIMER1_HDR_TIMER1_CONFIG_H_
+#define MCAL_TIMER1_HDR_TIMER1_CONFIG_H_
+
+#define T1_PrescalerSelect T1_Prescaler_8
+
+#endif /* MCAL_TIMER1_HDR_TIMER1_CONFIG_H_ */
