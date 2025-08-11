7170f290543824af4d8bfb971b89c18e466b2d06
Abdelwahab-Elordy <abdelwahabelordy@gmail.com>
2025-08-11 21:47:23 +0300

    Create Definition.h

---
+++ Lib/Definition.h
@@ -0,0 +1,41 @@
+/*
+ * Definition.h
+ *
+ *  Created on: Jul 17, 2025
+ *      Author: tohami
+ */
+
+#ifndef LIB_DEFINITION_H_
+#define LIB_DEFINITION_H_
+
+
+// Data types
+
+//char
+typedef unsigned char u8;
+typedef signed char s8;
+
+//int
+typedef short unsigned int u16;
+typedef short signed int s16;
+
+typedef unsigned int u32;
+typedef signed int s32;
+
+ //long signed int s32;
+ //long unsigned int u32;
+
+//float
+
+typedef float f32 ;
+//double
+typedef double f64 ;
+typedef long double f128 ;
+
+
+
+#define NULL (void*)0
+
+
+
+#endif /* LIB_DEFINITION_H_ */
